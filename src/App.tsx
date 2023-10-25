045cc1b2ccfa80f4b35d151cf8e1f08d4bfeb35f
Hoang Duy <hoangnhatbaduy2001@gmail.com>
2023-10-26 00:27:52 +0700

    up

---
+++ src/App.tsx
@@ -0,0 +1,241 @@
+import logo from "./assets/Logo-PMC.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="px-4">
+      <div className="mt-[150px] mb-[60px]">
+        <h2 className="text-center text-4xl mb-[50px] text-[#173e62]">
+          A name that has stood tall for <br /> over 30 years
+        </h2>
+        <p className="text-center text-base opacity-80">
+          ReHomes Group started it’s journey almost 3 decades back. The Group
+          has firmly <br />
+          established itself as a trustworthy and dependable organization inReal
+          Estate <br />
+          Industry that delivers quality.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 lg:gap-7 mb-[110px]">
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col group shadow-xl rounded-lg">
+          <div className="relative">
+            <img
+              src="https://demo2.wpopal.com/rehomes/wp-content/uploads/2019/10/about-1.jpg"
+              alt=""
+            />
+            <div className="bg-[#173e62] w-full h-full opacity-80 absolute top-0 group-hover:block hidden"></div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:block hidden">
+              <img src={logo} alt="" />
+            </div>
+          </div>
+          <div className="p-[30px] py-[40px]">
+            <span className="text-[#bda588] font-bold text-center block mb-1">
+              2017
+            </span>
+            <h5 className="block text-center text-[#173e62] font-bold text-xl">
+              National Real Estate <br /> Excellence Awards
+            </h5>
+            <div className="text-center mb-5 text-[#66717a]">
+              Best Luxury Real Estate Company
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 mb-10">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+        >
+          <SwiperSlide>
+            <div className="flex justify-between items-center flex-col">
+              <span className="text-[#bda588] text-sm">TESTIMONIALS </span>
+              <h2 className="block mt-[30px] pt-[30px] text-[#173e62] text-xl md:text-2xl lg:text-3xl text-center max-w-[60%] mb-[53px] font-semibold">
+                Gardenia by ReHomes Realty is an amazing project surrounded by
+                Mango orchard! We are really happy to have a home here.{" "}
+              </h2>
+              <div className="w-20 h-1 bg-[#BDA588] mb-4"></div>
+              <div className="text-[#173e62] text-lg lg:text-xl font-semibold">
+                John Doe
+              </div>
+              <div className="opacity-80 text-sm lg:text-base font-medium">
+                Design
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex justify-between items-center flex-col">
+              <span className="text-[#bda588] text-sm">TESTIMONIALS </span>
+              <h2 className="block mt-[30px] pt-[30px] text-[#173e62] text-xl md:text-2xl lg:text-3xl text-center max-w-[60%] mb-[53px] font-semibold">
+                Gardenia by ReHomes Realty is an amazing project surrounded by
+                Mango orchard! We are really happy to have a home here.{" "}
+              </h2>
+              <div className="w-20 h-1 bg-[#BDA588] mb-4"></div>
+              <div className="text-[#173e62] text-lg lg:text-xl font-semibold">
+                John Doe
+              </div>
+              <div className="opacity-80 text-sm lg:text-base font-medium">
+                Design
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex justify-between items-center flex-col">
+              <span className="text-[#bda588] text-sm">TESTIMONIALS </span>
+              <h2 className="block mt-[30px] pt-[30px] text-[#173e62] text-xl md:text-2xl lg:text-3xl text-center max-w-[60%] mb-[53px] font-semibold">
+                Gardenia by ReHomes Realty is an amazing project surrounded by
+                Mango orchard! We are really happy to have a home here.{" "}
+              </h2>
+              <div className="w-20 h-1 bg-[#BDA588] mb-4"></div>
+              <div className="text-[#173e62] text-lg lg:text-xl font-semibold">
+                John Doe
+              </div>
+              <div className="opacity-80 text-sm lg:text-base font-medium">
+                Design
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex justify-between items-center flex-col">
+              <span className="text-[#bda588] text-sm">TESTIMONIALS </span>
+              <h2 className="block mt-[30px] pt-[30px] text-[#173e62] text-xl md:text-2xl lg:text-3xl text-center max-w-[60%] mb-[53px] font-semibold">
+                Gardenia by ReHomes Realty is an amazing project surrounded by
+                Mango orchard! We are really happy to have a home here.{" "}
+              </h2>
+              <div className="w-20 h-1 bg-[#BDA588] mb-4"></div>
+              <div className="text-[#173e62] text-lg lg:text-xl font-semibold">
+                John Doe
+              </div>
+              <div className="opacity-80 text-sm lg:text-base font-medium">
+                Design
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default App;
